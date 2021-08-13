20b27643a43411195aafb1c8ce3eb82a7c76f10a
6mori <luhs1043@gmail.com>
2021-08-13 23:30:44 +0800

    feat: bat脚本输入文件夹扫描

--- xml2ass.bat
+++ xml2ass.bat
@@ -1,4 +1,14 @@
-SET INPUT_FILE=%*
+REM echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-python "danmaku2ass.py" %INPUT_FILE% -f Niconico -s 683x384 -o %INPUT_FILE:xml=ass% -fn ºÚÌå
+SET INPUT_DIR=%*
+SET XML_SUFFIX=*.xml
+
+for %%i in (%INPUT_DIR%%XML_SUFFIX%) do (
+    SET INPUT_FILE="%%i"
+    SET OUTPUT_FILE=!INPUT_FILE:xml=ass!
+    if not exist OUTPUT_FILE (
+        python "danmaku2ass.py" !INPUT_FILE! -f Niconico -s 683x384 -o !OUTPUT_FILE! -fn ºÚÌå
+    )
+)
 pause
